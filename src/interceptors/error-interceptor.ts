cdf96ea95dc8b2d26cbcae024be5010860716762
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-10-03 11:25:46 -0300

    Tratamento 404 e padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,15 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS
 import { Observable } from "rxjs/Rx";
 import { Injectable } from "@angular/core";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
 
-    constructor(public storage: StorageService){}
+    constructor(
+        public storage: StorageService,
+        public alertCtrl: AlertController
+        ){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
         return next.handle(req)
@@ -27,6 +31,18 @@ export class ErrorInterceptor implements HttpInterceptor{
                     case 403:
                         this.handle403();
                         break;
+
+                    case 401:
+                        this.handle401();
+                        break;
+
+                    case 404:
+                        this.handle404();
+                        break;
+
+                    default:  
+                        this.hendleDefaultError(errorObj);
+                        break;  
                 }
 
                 return Observable.throw(errorObj);
@@ -35,6 +51,48 @@ export class ErrorInterceptor implements HttpInterceptor{
 
     handle403(){
         this.storage.setLocalUser(null);
+    }
+
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401, Falha na autenticação',
+            message: 'Email ou senha incorreta',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handle404(){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 404, Não encontrado',
+            message: 'Não foi possível encontrar o conteúdo',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    hendleDefaultError(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'OK'
+                }
+            ]
+        });
+        alert.present();
     }
 
 }
