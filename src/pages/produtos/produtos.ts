d4a523bb868784e0c61e0703bd31563a7d618c18
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-10-05 11:01:19 -0300

    Carregando produtos por Categoria

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../dto/models/produto.dto';
+import { ProdutoService } from '../../services/domain/produto.service';
 
 
 @IonicPage()
@@ -12,22 +13,17 @@ export class ProdutosPage {
 
   items: ProdutoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public produtoService: ProdutoService) {}
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        nome: "Mouse",
-        preco: 80.99
+    this.produtoService.findByCategoria(this.navParams.get('categoria_id'))
+      .subscribe(response =>{
+        this.items = response['content'];
       },
-      {
-        id: "2",
-        nome: "Teclado",
-        preco: 100.00
-      }
-    ]
+      error=>{});
   }
 
 }
