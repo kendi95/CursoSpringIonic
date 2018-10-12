4ebe4a7455248c9d268574035666aada4fe58dec
Alisson Kohatsu <kohatsukendi@gmail.com>
2018-10-12 16:35:44 -0300

    Acesso ao carrinho no menu e no botão flutuante

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 import { ProdutoService } from '../services/domain/produto.service';
 import { CartService } from '../services/domain/cart.service';
 
+
 @NgModule({
   declarations: [
     MyApp
